b9b8d461b5baf564b45da6d04893d34a9aa64622
roseviet <uyen.nguyencong@gmail.com>
2023-08-11 21:56:27 +0700

    support layout

--- frontend/uz-app/src/pages/_app.tsx
+++ frontend/uz-app/src/pages/_app.tsx
@@ -1,13 +1,21 @@
+import { NextPage } from "next";
 import type { AppProps } from "next/app";
+import React, { ReactNode, ReactElement } from "react";
 
 import { Providers as AppProvider } from "@/providers/app";
 
-const App = ({ Component, pageProps }: AppProps) => {
-  return (
-    <AppProvider>
-      <Component {...pageProps} />
-    </AppProvider>
-  );
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+const App = ({ Component, pageProps }: AppPropsWithLayout) => {
+  const getLayout = Component.getLayout ?? ((page) => page);
+  const pageContent = getLayout(<Component {...pageProps} />);
+  return <AppProvider>{pageContent}</AppProvider>;
 };
 
 export default App;
--- frontend/uz-app/src/providers/app.tsx
+++ frontend/uz-app/src/providers/app.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import React from "react";
 import { CacheProvider } from "@chakra-ui/next-js";
 import { ChakraProvider, GlobalStyle } from "@chakra-ui/react";
 
