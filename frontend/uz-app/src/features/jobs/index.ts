83e9cd3cf93c90ce5d8a61176f17f2672f73f270
roseviet <uyen.nguyencong@gmail.com>
2023-08-11 23:41:14 +0700

    add pages dashboard jobs

--- frontend/uz-app/src/features/jobs/index.ts
+++ frontend/uz-app/src/features/jobs/index.ts
@@ -1,4 +1,6 @@
 // components:
+export * from "./components/create-job-form";
+export * from "./components/dashboard-job-info";
 export * from "./components/jobs-list";
 export * from "./components/public-job-info";
 
--- frontend/uz-app/src/pages/index.tsx
+++ frontend/uz-app/src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { Center, Heading, Text, VStack } from "@chakra-ui/react";
 
+import { Link } from "@/components/link";
 import { Seo } from "@/components/seo";
 
 const LandingPage = () => {
@@ -12,6 +13,9 @@ const LandingPage = () => {
           <Text fontSize={{ base: "lg", md: "xl" }} maxW="2xl" color="muted">
             The custom app for UZ
           </Text>
+          <Link href={"/dashboard/jobs"} variant="solid">
+            Go to Sample Dashboard Jobs
+          </Link>
         </VStack>
       </Center>
     </>
