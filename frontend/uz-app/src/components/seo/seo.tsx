b5200bf9058b1d0f0c7b25d4afdfc8c97a6a9737
roseviet <uyen.nguyencong@gmail.com>
2023-08-11 19:06:44 +0700

    support SEO

---
+++ frontend/uz-app/src/components/seo/seo.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Head from "next/head";
+
+export type SeoProps = {
+  title: string;
+};
+
+export const Seo = ({ title }: SeoProps) => {
+  return (
+    <Head>
+      <title>{title}</title>
+    </Head>
+  );
+};
--- frontend/uz-app/src/pages/index.tsx
+++ frontend/uz-app/src/pages/index.tsx
@@ -1,17 +1,19 @@
-import { Button } from "@/components/button";
-import { InputField } from "@/components/form";
-import { Link } from "@/components/link";
+import { Center, Heading, Text, VStack } from "@chakra-ui/react";
+
+import { Seo } from "@/components/seo";
 
 const LandingPage = () => {
   return (
     <>
-      <Button variant="solid" type="button">
-        Click Me
-      </Button>
-      <br />
-      <InputField label="Name" />
-      <br />
-      <Link href="/">Home</Link>
+      <Seo title="UZ App" />
+      <Center flexDirection="column" h="full">
+        <VStack maxW="3xl" spacing="8">
+          <Heading size="3xl">UZ APP</Heading>
+          <Text fontSize={{ base: "lg", md: "xl" }} maxW="2xl" color="muted">
+            The custom app for UZ
+          </Text>
+        </VStack>
+      </Center>
     </>
   );
 };
